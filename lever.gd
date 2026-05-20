90557079ad9434011c1c81153091b50c97b9e80d
Lawrence Mathew Pedarse Seeberger <lawrencemathewseeberger@gmail.com>
2026-05-19 19:16:14 -0700

    LEVER WROKS CORRECTLY
    YES

--- lever.gd
+++ lever.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if rotatable==true:
 		leverposition=5*(player.position.x-position.x)
 		pull.rotation_degrees+=(leverposition/10)
--- manual_platform_small.gd
+++ manual_platform_small.gd
@@ -3,14 +3,25 @@ extends AnimatableBody2D
 @onready var cog: AnimatedSprite2D = $Cog
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var positionment: float
-
+var gotime: float
+var positioner: float = absf(LevelManager.leverlevel)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	LevelManager.leverlevel=0
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	animation_player.advance(LevelManager.leverlevel)
-	
+func _process(delta: float) -> void:
+	positioner= LevelManager.leverlevel
+	var converter: float= (positioner+90)/180
+	if animation_player:
+		animation_player.play("back")
+		if !absf(animation_player.current_animation_position)==converter:
+			animation_player.advance(4*(converter-animation_player.current_animation_position)*delta)
+		if animation_player.current_animation_position>0.99:
+			LevelManager.leverlevel=89.9
+		if absf(animation_player.current_animation_position-converter)> 0.05:
+			cog.play("default")
+		else:
+			cog.play("stopped")
 	
